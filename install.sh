d630d04ae0a5dcb755a03a7d903c2a60040f5eed
Dhiky Can <48305398+dhiky@users.noreply.github.com>
2024-01-02 11:53:12 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,13 +5,21 @@ db_passwd="$(cat /dev/urandom | tr -dc '[:alnum:]!@#$%^&*()' | head -c 12)"
 db_user="wp_data$(printf '%02d' $((RANDOM % 100)))"
 user_admin="wp_admin$(printf '%02d' $((RANDOM % 100)))"
 user_passwd="$(cat /dev/urandom | tr -dc '[:alnum:]!@#$%^&*()' | head -c 10)"
+mysql_passwd=$1
 
 # Install the Wordpress
 echo "Installing Wordpress.."
 doc_root="$(sed -n -e '/^\s*root\s*/{s/^\s*root\s*//;s/;//p}' /etc/nginx/conf.d/*.conf)"
 wp core download --path=$doc_root
-wp config create --dbname=$db_user --dbuser=$db_user --dbpass=$db_passwd --path=$doc_root
 echo "Creating Database for Wordpress..."
+mysql -u root -p'$mysql_passwd' << EOF
+CREATE DATABASE $db_user;
+CREATE USER $db_user@localhost IDENTIFIED BY '$db_passwd';
+GRANT SELECT, CREATE, DELETE, INSERT, UPDATE ON $db_user.* TO '$db_user'@'localhost';
+FLUSH PRIVILEGES;
+EXIT;
+EOF
+wp config create --dbname=$db_user --dbuser=$db_user --dbpass=$db_passwd --path=$doc_root
 wp db create --path=$doc_root
 wp core install --url="http://bokunoweb.my.id" --title="Your Website" --admin_user=$user_admin --admin_password=$user_passwd --admin_email=dhiky.cancerio@gmail.com --path=$doc_root
 echo "We are successfully installing the Wordpress, Please use this Creds and login on your web."
